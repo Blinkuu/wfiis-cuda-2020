ce97e0424c299850909cf54af5f750ceeb86fd57
cuda lab05 <cuda-lab05@nat110-fix-221.fis.agh.edu.pl>
2020-03-27 11:30:41 +0100

    Change to 16x16 grid configuration

--- rtx-matrix-operations/src/configs/config.h
+++ rtx-matrix-operations/src/configs/config.h
@@ -4,6 +4,6 @@ namespace cuda {
 	struct config {
 		static constexpr std::size_t max_iterations = 10;
 		static constexpr std::size_t num_elements = 2048;
-		static constexpr std::size_t threads_per_block = 32;
+		static constexpr std::size_t threads_per_block = 16;
 	};
 }
